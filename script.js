8df647defee568cd4c9e4bb0f4fdd9f8caecec21
cstt1 <cstt@gmail.com>
2026-02-11 11:32:14 +0530

    completed customize page with interactive preview

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Elegance Wraps Website Loaded Successfully!");
+
+const form = document.getElementById("customForm");
+const giftBox = document.getElementById("giftPreview");
+const previewMessage = document.getElementById("previewMessage");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    const color = document.getElementById("color").value;
+    const ribbon = document.getElementById("ribbon").value;
+    const message = document.getElementById("message").value;
+
+    giftBox.style.backgroundColor = color;
+    giftBox.style.border = `5px solid ${ribbon}`;
+    previewMessage.textContent = message;
+});
